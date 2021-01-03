40bddf5bdac97e587db850f4de7dcde09aeba599
lite-panel-4424 <barneschristopherh@gmail.com>
2021-01-03 22:42:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,6 @@
-# Write your code here.
+katz_deli = []
+def take_a_number(katz_deli,name = "The line is currently empty.")
+  puts "Welcome, #{name}. You are number #{katz_deli} in line."
+end
+
+take_a_number(katz_deli)
